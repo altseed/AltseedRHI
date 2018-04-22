06adee3a41afd6f5e13f52086177c7a1a143ac47
durswd <swda.durl@gmail.com>
2018-04-23 01:03:24 +0900

    Updated includes

--- include/AltseedRHI.h
+++ include/AltseedRHI.h
@@ -661,6 +661,8 @@ namespace ar
 
 		TextureType GetType() const override { return TextureType::Texture2D; }
 
+		virtual uint64_t GetInternalObject() const { return 0; }
+
 		static Texture2D* Create(Manager* manager);
 	};
 
